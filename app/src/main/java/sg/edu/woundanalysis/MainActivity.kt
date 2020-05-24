708e9f4feecd7d6436a1f2d75e3e7a19f852755a
kschiew <kschiew98@gmail.com>
2020-05-24 17:36:27 +0800

    Add permission control

--- app/src/main/java/sg/edu/woundanalysis/MainActivity.kt
+++ app/src/main/java/sg/edu/woundanalysis/MainActivity.kt
@@ -1,18 +1,20 @@
 package sg.edu.woundanalysis
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.Camera
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.util.concurrent.ExecutorService
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -28,31 +30,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        appbar.setTitle(R.string.app_name)
+
         // Request camera permission
         ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE_PERMISSIONS)
 
         // Listener for take photo button
-        btn_capture.setOnClickListener( capturePhoto() )
+        btn_capture.setOnClickListener(capturePhoto())
 
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    private fun capturePhoto(): View.OnClickListener? {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                        "Permission not granted by user",
+                        Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
 
+    private fun capturePhoto(): View.OnClickListener? {
+        return null;
     }
 
     private fun getOutputDirectory(): File {
-       val mediaDir = externalMediaDirs.firstOrNull()?.let {
-           File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
         return if (mediaDir != null && mediaDir.exists())
             mediaDir else filesDir
+    }
+
+    private fun allPermissionsGranted() = PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        // todo
     }
 
 
     companion object {
         private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val TAG = R.string.app_name
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
 
     }
 
