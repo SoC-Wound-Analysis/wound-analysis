ca9f71a8243f517effa83f0618003bccacf4140d
kschiew <kschiew98@gmail.com>
2020-05-26 03:44:27 +0800

    Implement camera2 api

--- app/src/main/java/sg/edu/woundanalysis/MainActivity.kt
+++ app/src/main/java/sg/edu/woundanalysis/MainActivity.kt
@@ -1,31 +1,55 @@
 package sg.edu.woundanalysis
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import android.util.Log
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.TextureView
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
+import androidx.camera.core.Camera
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    //Useless CameraX variables
     private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
     private var imageAnalyzer: ImageAnalysis? = null
-    private var camera: Camera? = null
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    //Creates Texture View for Camera Preview
+    private var mTextureView: TextureView? = null
+    private var camera: Camera? = null
+
+    val surfaceReadyCallback = object: SurfaceHolder.Callback {
+
+        // Unused listeners
+        override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {}
+        override fun surfaceDestroyed(holder: SurfaceHolder?) {}
+
+        override fun surfaceCreated(holder: SurfaceHolder?) {
+            startCamera()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +60,15 @@ class MainActivity : AppCompatActivity() {
         // Request camera permission
         ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE_PERMISSIONS)
 
+       // Set surfaceView's callback
+        surfaceView.holder.addCallback(surfaceReadyCallback)
+
         // Listener for take photo button
         btn_capture.setOnClickListener(capturePhoto())
 
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
+
     }
 
     /**
@@ -48,9 +76,7 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
+            if (!allPermissionsGranted()) {
                 Toast.makeText(this,
                         "Permission not granted by user",
                         Toast.LENGTH_SHORT).show()
@@ -79,38 +105,97 @@ class MainActivity : AppCompatActivity() {
      * Initializes the camera device with CameraX API.
      */
     private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val tofCamera = cameraManager.cameraIdList[0]
 
-        cameraProviderFuture.addListener(Runnable {
-            // Bind the camera to lifecycle
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+        Log.d(TAG, "Hello World!")
+        Log.d(TAG, cameraManager.cameraIdList[4])
 
-            // Preview
-            preview = Preview.Builder().build()
+        if (ActivityCompat.checkSelfPermission(baseContext,
+                        android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
 
-            // Select the camera used
-            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        cameraManager.openCamera(tofCamera, object:
+                CameraDevice.StateCallback() {
 
-            try {
-                //Unbind before rebinding
-                cameraProvider.unbindAll()
+            // Unused listeners
+            override fun onDisconnected(camera: CameraDevice) {}
+            override fun onError(camera: CameraDevice, error: Int) {}
 
-                // Bind to camera
-                camera = cameraProvider.bindToLifecycle(
-                        this, cameraSelector, preview)
-                preview?.setSurfaceProvider(viewPreview.createSurfaceProvider(camera?.cameraInfo))
-            } catch(e: Exception) {
-                Log.e(TAG, "Use case binding failed", e)
+            override fun onOpened(cameraDevice: CameraDevice) {
+                val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraDevice.id)
+
+                cameraCharacteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]?.let { streamConfigurationMap ->
+
+                    streamConfigurationMap.getOutputSizes(ImageFormat.JPEG)?.let { yuvSizes ->
+                        val previewSize = yuvSizes.last()
+
+                        val displayRotation = windowManager.defaultDisplay.rotation
+                        val swappedDimensions = areDimensionsSwapped(displayRotation, cameraCharacteristics)
+
+                        // Choose the correct width and height
+                        val rotatedPreviewHeight = if (swappedDimensions) previewSize.width else previewSize.height
+                        val rotatedPreviewWidth = if (swappedDimensions) previewSize.height else previewSize.width
+                    }
+                }
+
+                // I dunno where to put this part
+                val previewSurface = surfaceView.holder.surface
+
+                val captureCallback = object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigureFailed(session: CameraCaptureSession) {}
+
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        val previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
+                            addTarget(previewSurface)
+                        }
+
+                        session.setRepeatingRequest(
+                                previewRequestBuilder.build(),
+                                object: CameraCaptureSession.CaptureCallback() {},
+                                android.os.Handler {true}
+                        )
+                    }
+                }
+
+                cameraDevice.createCaptureSession(mutableListOf(previewSurface),
+                captureCallback, android.os.Handler {true})
+
             }
-        }, ContextCompat.getMainExecutor(this))
+
+        }, android.os.Handler {true})
     }
 
+    /**
+     * Helper function to determine and adjust the preview according to the orientation of the camera.
+     */
+    private fun areDimensionsSwapped(displayRotation: Int, cameraCharacteristics: CameraCharacteristics): Boolean {
+        var swappedDimensions = false
+        when (displayRotation) {
+            Surface.ROTATION_0, Surface.ROTATION_180 -> {
+                if (cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 90 || cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 270) {
+                    swappedDimensions = true
+                }
+            }
+            Surface.ROTATION_90, Surface.ROTATION_270 -> {
+                if (cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 0 || cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 180) {
+                    swappedDimensions = true
+                }
+            }
+            else -> {
+                // Impossible to reach this part of code
+            }
+        }
+        return swappedDimensions
+    }
 
     companion object {
         private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_PERMISSIONS = 10
         private const val TAG = R.string.app_name.toString()
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val TOF_CAMERA_ID = 4
 
     }
 
